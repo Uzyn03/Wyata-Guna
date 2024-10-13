ad0f30511002eba58146b32fc5f3f721956ee883
Ahmad Fauzi <ahmadpjn06@gmail.com>
2024-10-13 11:45:55 +0700

    revisi

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 // toggle class active
 const navbarNav = document.querySelector('.navbar-right');
 
-document.querySelector('#menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+
+const menu = document.querySelector(".navbar-extra");
+const hamburgerIcon = document.querySelector("#menu");
+const closeIcon = document.querySelector("#close");
+
+menu.onclick = () => {
+  navbarNav.classList.toggle("active");
+
+  // Cek apakah menu aktif atau tidak
+  const isActive = navbarNav.classList.contains("active");
+  hamburgerIcon.style.display = isActive ? "none" : "block";
+  closeIcon.style.display = isActive ? "block" : "none";
 };
 
-const menu = document.querySelector('#menu');
-document.addEventListener('click', function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-      navbarNav.classList.remove('active');
-    }
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+    hamburgerIcon.style.display = "block";
+    closeIcon.style.display = "none";
+  }
 });
