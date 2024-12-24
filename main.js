a21ff0ce5fbf18d1b5695cb4e26fb721b4006b7a
Ahmad Fauzi <ahmadpjn06@gmail.com>
2024-12-24 23:21:23 +0700

    penambahan halaman kegiatan

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // toggle class active
-const navbarNav = document.querySelector('.navbar-right');
-
+const navbarNav = document.querySelector(".navbar-right");
 
 const menu = document.querySelector(".navbar-extra");
 const hamburgerIcon = document.querySelector("#menu");
@@ -21,4 +20,52 @@ document.addEventListener("click", function (e) {
     hamburgerIcon.style.display = "block";
     closeIcon.style.display = "none";
   }
+});
+
+// Kegiatan
+// Ambil semua elemen dengan class "kegiatan"
+const kegiatanElements = document.querySelectorAll(".kegiatan");
+
+// Tambahkan event listener untuk setiap elemen
+kegiatanElements.forEach((kegiatan) => {
+  kegiatan.addEventListener("click", () => {
+    // Tambahkan efek visual (class clicked)
+    kegiatan.classList.add("clicked");
+
+    // Hapus efek setelah animasi selesai (300ms sesuai dengan CSS)
+    setTimeout(() => {
+      kegiatan.classList.remove("clicked");
+    }, 200);
+
+    // Arahkan ke halaman yang sesuai
+    switch (kegiatan.id) {
+      case "vokasional":
+        setTimeout(() => {
+          window.location.href = "vokasional.html";
+        }, 200); // Tunggu animasi selesai sebelum pindah halaman
+        break;
+      case "kamis":
+        setTimeout(() => {
+          window.location.href = "kamis.html";
+        }, 200);
+        break;
+      case "terminasi":
+        setTimeout(() => {
+          window.location.href = "terminasi.html";
+        }, 200);
+        break;
+      case "senam":
+        setTimeout(() => {
+          window.location.href = "senam.html";
+        }, 200);
+        break;
+      case "rabu":
+        setTimeout(() => {
+          window.location.href = "rabu.html";
+        }, 200);
+        break;
+      default:
+        console.log("Halaman tidak ditemukan!");
+    }
+  });
 });
